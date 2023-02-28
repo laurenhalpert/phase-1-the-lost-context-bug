4b1b0ffd7099ccd3433f8738d86bfb621793fc5f
Lauren <halpertln@gmail.com>
2023-02-28 13:24:37 -0700

    complete lab

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+console.log(messageConfig.closing.Thor);
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  // Wow! Elegant! And notice the arrow function's `this` is the same `this`
+  // that printCard has; specifically, the `thisArg` that was passed to it
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
+
+const greeter = (nameToGreet) => {
+  const message = `Good morning ${nameToGreet}`;
+  console.log(message);
+  return "Greeted: " + nameToGreet;
+};
+const result = greeter("Max");
+
+
+
